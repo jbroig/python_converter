dcb08a177d377803f42918ccd2c0255a361fe9b9
Juan Bravo Roig <juanbravoroig@gmail.com>
2018-05-17 16:01:23 +0200

    Update main.py

--- converter.py
+++ converter.py
@@ -4,10 +4,9 @@ import platform
 import os
 import time
 
-original_file = open("origen.csv", "r")
-new_file = open("new_csv.csv","w")
-
 def converter():
+    original_file = open("TIAMO_EXPORT.csv", "r")
+    new_file = open("TIAMO_EXPORT_UPLOAD.csv","w")
     for row in original_file:
 
         original_row = re.sub(r',','.', row)
@@ -20,21 +19,29 @@ def file_dates(path_to_csv, path_to_converter ):
         converter_modification_date = time.ctime(os.path.getmtime(path_to_converter))
 
         csv_modification_date = csv_modification_date.split(":")      
-        csv_modification_date = ":".join(csv_modification_date[:-1])
+        csv_modification_date = str(":".join(csv_modification_date[:-1]))
         
         converter_modification_date = converter_modification_date.split(":")
-        converter_modification_date = ":".join(converter_modification_date[:-1])
+        converter_modification_date = str(":".join(converter_modification_date[:-1]))
 
+        s_time = time.ctime()
+        s_time = s_time.split(":")
+        s_time = str(":".join(s_time[:-1]))
 
-        if (csv_modification_date != converter_modification_date):
+        print("Time: {0}".format(s_time))
+        print("Last converter modification: {0}".format(converter_modification_date))
+
+        if (s_time != converter_modification_date):
             print("Executing...")
             converter()
             
-            
-        print("Last csv modification: %s" % csv_modification_date)
-        print("Last converter modification: %s" % converter_modification_date)
+            os.system("gdrive update 1HQM1QUSSMDgE5-nZ5m2UaBaMwDWv_ZkBX8LdzHCR4Pc TIAMO_EXPORT_UPLOAD.csv")
+        else:
+            print("Everything is updated")    
+
+        
 
 
 
 
-file_dates(r'C:\\Users\Administrador\\Desktop\\a.txt','C:\\Users\\Administrador\\Desktop\\b.txt')
+file_dates('TIAMO_EXPORT.csv','TIAMO_EXPORT_UPLOAD.csv')
